dd7ac1f96027c676a0852610bb23a1e22f08dd3c
Anuja Tappeta <153365922+anujatappeta@users.noreply.github.com>
2026-03-27 23:13:42 +0530

    Clean up collaborative_based.py by removing comments
    Removed commented sections for clarity and conciseness.

--- ai_recom/backend/collaborative_based.py
+++ ai_recom/backend/collaborative_based.py
@@ -14,10 +14,7 @@ def get_collaborative_recommendations(user_id, top_n=10):
 
         if df.empty:
             return []
-
-        # -------------------------
         # USER-ITEM MATRIX
-        # -------------------------
         user_item_matrix = df.pivot_table(
             index="UserID",
             columns="ProductID",
@@ -27,10 +24,7 @@ def get_collaborative_recommendations(user_id, top_n=10):
         # If user not present → return empty
         if user_id not in user_item_matrix.index:
             return []
-
-        # -------------------------
         # SIMILARITY
-        # -------------------------
         user_similarity = cosine_similarity(user_item_matrix)
 
         user_similarity_df = pd.DataFrame(
@@ -38,18 +32,13 @@ def get_collaborative_recommendations(user_id, top_n=10):
             index=user_item_matrix.index,
             columns=user_item_matrix.index
         )
-
-        # -------------------------
         # FIND SIMILAR USERS
-        # -------------------------
         similar_users = user_similarity_df[user_id].sort_values(ascending=False)
         similar_users = similar_users.drop(user_id)
 
         top_similar_users = similar_users.head(5).index
-
-        # -------------------------
         # RECOMMENDATION SCORE
-        # -------------------------
+        
         similar_users_ratings = user_item_matrix.loc[top_similar_users]
 
         recommendation_scores = similar_users_ratings.mean(axis=0)
@@ -58,9 +47,7 @@ def get_collaborative_recommendations(user_id, top_n=10):
 
         recommendation_scores = recommendation_scores[user_rated_products == 0]
 
-        # -------------------------
         # TOP PRODUCTS
-        # -------------------------
         top_products = recommendation_scores.sort_values(ascending=False).head(top_n)
 
         return top_products.index.astype(int).tolist()
