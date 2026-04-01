dda3d60791185188ff5d60c8430aebc5748b02f3
Anuja Tappeta <153365922+anujatappeta@users.noreply.github.com>
2026-04-01 17:32:12 +0000

    Updated requirements.txt and config

--- rxconfig.py
+++ rxconfig.py
@@ -1,7 +1,9 @@
+import os
 import reflex as rx
 
 config = rx.Config(
     app_name="ai_recom",
+    api_url=os.environ.get("REFLEX_API_URL") or "http://127.0.0.1:8000",
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
